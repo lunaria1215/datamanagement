0da18e77c3e47477d1f4e124713c6c1818d06edc
lunaria1215 <s1310130@u-aizu.ac.jp>
2024-07-04 21:43:29 +0900

    HoT: initial revisio

---
+++ coin.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define OVERLOAD_REP(_1, _2, _3, name, ...) name
+#define REP1(i, n) for (auto i = decay_t<decltype(n)>{}; (i) != (n); ++(i))
+#define REP2(i, l, r) for (auto i = (l); (i) != (r); ++(i))
+#define rep(...) OVERLOAD_REP(__VA_ARGS__, REP2, REP1)(__VA_ARGS__)
+#define repr(i, n) for (int i = n; i >= 0; i--)
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define vcin(x) \
+    for (auto &youso_ : (x)) cin >> youso_
+#define yes cout << "yes" << endl
+#define Yes cout << "Yes" << endl
+#define YES cout << "YES" << endl
+#define no cout << "no" << endl
+#define No cout << "No" << endl
+#define NO cout << "NO" << endl
+const int MOD = 998244353;
+const int INF = 1000000000;
+const long long LINF = 1e18;
+using ll = long long;
+template <class T>
+using v1 = vector<T>;
+template <class T>
+using v2 = vector<vector<T>>;
+template <class T>
+using v3 = vector<vector<vector<T>>>;
+template <typename T>
+bool chmax(T &a, const T &b) {
+    return (a < b ? (a = b, true) : false);
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+    return (a > b ? (a = b, true) : false);
+}
+template <class T>
+ostream &operator<<(ostream &os, vector<T> v) {
+    for (int i = 0; i < (int)v.size(); i++) {
+        if (i) os << " ";
+        os << v[i];
+    }
+    return os;
+}
+typedef pair<int, int> p;
+
+int main() {
+    cout << "Tossing a coin..." << endl;
+    int cnt1 = 0, cnt2 = 0;
+    rep(i, 1, 4) {
+        cout << "Round " << i;
+        int cnt = rand() % 2;
+        if (cnt == 0) {
+            cout << ": Heads" << endl;
+            cnt1++;
+        } else {
+            cout << ": Tails" << endl;
+            cnt2++;
+        }
+    }
+    cout << "Heads: " << cnt1 << ", Tails: " << cnt2 << endl;
+}
